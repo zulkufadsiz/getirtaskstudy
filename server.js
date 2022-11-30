73a016d3daf8a55b6a7469f0c32d9d39fbf0d46c
zulkufadsiz <zulkufadsiz@gmail.com>
2022-11-30 11:18:45 +0100

    Vercel.json added.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./src/utils/dummy-data/db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
   static: './build'
 });
